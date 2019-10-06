5c2b2bb1df9d7e2c21ae1927e3d6aa31a23b272f
nigmatullin-trpo <nigmatullin-trpo@mail.ru>
2019-10-07 02:54:50 +0700

    New test has been added

--- test/verbs_test.c
+++ test/verbs_test.c
@@ -9,3 +9,73 @@ CTEST(mark_suite, below_zero_test)
 	const int expected = 0;
 	ASSERT_EQUAL(expected, result);
 }
+
+CTEST(mark_suite, zero_test)
+{
+	const int result = write_mark(amount, 0);
+	const int expected = 0;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_two_left_test)
+{
+	const int result = write_mark(amount, 1);
+	const int expected = 2;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_two_right_test)
+{
+	const int result = write_mark(amount, 35);
+	const int expected = 2;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_three_left_test)
+{
+	const int result = write_mark(amount, 36);
+	const int expected = 3;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_three_right_test)
+{
+	const int result = write_mark(amount, 60);
+	const int expected = 3;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_four_left_test)
+{
+	const int result = write_mark(amount, 61);
+	const int expected = 4;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_four_right_test)
+{
+	const int result = write_mark(amount, 85);
+	const int expected = 4;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_five_left_test)
+{
+	const int result = write_mark(amount, 86);
+	const int expected = 5;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_five_right_test)
+{
+	const int result = write_mark(amount, 100);
+	const int expected = 5;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, over_100_test)
+{
+	const int result = write_mark(amount, 101);
+	const int expected = 0;
+	ASSERT_EQUAL(expected, result);
+}
